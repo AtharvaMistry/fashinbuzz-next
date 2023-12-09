101c7a17afbd81222933e3aca4e3064d59119257
Jaimeen Makavana <129974169+AtharvaMistry@users.noreply.github.com>
2023-12-09 17:50:20 +0530

    latest

--- app/about/page.js
+++ app/about/page.js
@@ -5,9 +5,9 @@ function page() {
         <>
             <div className="grid px-5 text-center md:px-10">
                 <p className="text-4xl font-semibold">Our Journey - From Fabrics to Digitise Fashion</p>
-                <p class="mb-3 text-[#464646] mt-12  dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">For the past five years, FashinBuzz has been a trusted name in the textile industry, offering top-notch design, manufacturing, and delivery services. We've been the driving force behind many successful fashion endeavors, and now, we're excited to take our expertise online.</p>
-                <p class="text-[#464646] dark:text-gray-400">Our story begins with a passion for textiles and a commitment to quality. Over the years, we've honed our skills, partnered with talented designers, and built a robust manufacturing and delivery network that ensures excellence every step of the way.</p>
-                <p class="text-[#464646] dark:text-gray-400">Today, we're proud to introduce our online platform, where you can experience the same dedication to quality and innovation that has defined us for half a decade. Join us on this new chapter of our journey as we bring fabrics to life and fashion to your doorstep.</p>
+                <p class="mb-3 text-[#464646] mt-12  dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:me-3 first-letter:float-start">For the past five years, FashinBuzz has been a trusted name in the textile industry, offering top-notch design, manufacturing, and delivery services. We&apos;ve been the driving force behind many successful fashion endeavors, and now, we&apos;re excited to take our expertise online.</p>
+                <p class="text-[#464646] dark:text-gray-400">Our story begins with a passion for textiles and a commitment to quality. Over the years, we&apos;ve honed our skills, partnered with talented designers, and built a robust manufacturing and delivery network that ensures excellence every step of the way.</p>
+                <p class="text-[#464646] dark:text-gray-400">Today, we&apos;re proud to introduce our online platform, where you can experience the same dedication to quality and innovation that has defined us for half a decade. Join us on this new chapter of our journey as we bring fabrics to life and fashion to your doorstep.</p>
                 <p class="text-[#464646] dark:text-gray-400">Explore our website to discover the endless possibilities we offer, and be a part of our story as we continue to weave fashion dreams into reality.</p>
             </div>
             <div className="grid grid-cols-1 gap-5 px-5 my-10  md:grid-cols-2 md:px-10">
@@ -30,7 +30,7 @@ function page() {
                     <div className="container max-w-xl mx-auto">
                         <div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-900 dark:text-gray-100">
 
-                            <blockquote className="max-w-lg text-lg italic font-medium text-center">"Meet Abhay Raviya(CEO)a mechanical engineer with a deep-rooted passion for textiles, leads our journey at FashinBuzz. With a wealth of knowledge in textile technology and a keen eye for cloth design manufacturing, Abhay brings expertise and dedication to every aspect of our business. His commitment to quality and innovation forms the bedrock of our company's success. Trust in his vision to deliver excellence in every thread"</blockquote>
+                            <blockquote className="max-w-lg text-lg italic font-medium text-center">Meet Abhay Raviya(CEO)a mechanical engineer with a deep-rooted passion for textiles, leads our journey at FashinBuzz. With a wealth of knowledge in textile technology and a keen eye for cloth design manufacturing, Abhay brings expertise and dedication to every aspect of our business. His commitment to quality and innovation forms the bedrock of our company&apos;s success. Trust in his vision to deliver excellence in every thread.</blockquote>
                             <div className="text-center dark:text-gray-400">
                                 <p> Abhay Raviya</p>
                                 <p>CEO of Company Co.</p>
@@ -49,7 +49,7 @@ function page() {
                     <div className="container max-w-xl mx-auto">
                         <div className="flex flex-col items-center w-full p-6 space-y-8 rounded-md lg:h-full lg:p-8 dark:bg-gray-900 dark:text-gray-100">
 
-                            <blockquote className="max-w-lg text-lg italic font-medium text-center">"Meet Anubhav(CTO), a textile engineer by degree and a entreprenuer by heart, leads the charge as our Chief Technology Officer (CTO). With a profound understanding of AI and technology, he infuses innovation into every aspect of our operations. in ge's vision to integrate cutting-edge technology seamlessly into our processes, making your experience with us truly exceptional."</blockquote>
+                            <blockquote className="max-w-lg text-lg italic font-medium text-center">Meet Anubhav(CTO), a textile engineer by degree and a entreprenuer by heart, leads the charge as our Chief Technology Officer (CTO). With a profound understanding of AI and technology, he infuses innovation into every aspect of our operations. in ge&apos;s vision to integrate cutting-edge technology seamlessly into our processes, making your experience with us truly exceptional.</blockquote>
                             <div className="text-center dark:text-gray-400">
                                 <p>Anubhav vasistha</p>
                                 <p>CTO of Company Co.</p>
