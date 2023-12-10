57050f10487347e45ab7c0d0a8c7f438cd4487df
Jaimeen Makavana <129974169+AtharvaMistry@users.noreply.github.com>
2023-12-10 10:35:00 +0530

    latest changes

--- Components/HomeData.js
+++ Components/HomeData.js
@@ -22,13 +22,6 @@ function HomeData() {
                     </div>
 
                     <div className="stat">
-                        <div className="stat-figure text-secondary">
-                            <div className="avatar online">
-                                <div className="w-16 rounded-full">
-                                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                                </div>
-                            </div>
-                        </div>
                         <div className="stat-value text-[#333333]">100</div>
                         <div className="stat-desc text-secondary">Minimum order quantity</div>
                     </div>
