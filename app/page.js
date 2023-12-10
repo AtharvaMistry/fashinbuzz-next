0dfa49ec07fae4a2e8b6c758e371b22f90d91c6b
Jaimeen Makavana <129974169+AtharvaMistry@users.noreply.github.com>
2023-12-10 10:37:36 +0530

    latest changes

--- app/page.js
+++ app/page.js
@@ -20,8 +20,21 @@ export default function Home() {
         </div>
 
         {/* image carousel */}
-        <IntroCarousel />
-
+        {/* <IntroCarousel /> */}
+        <div className="grid px-5 my-10 md:px-10">
+          <div className="carousel w-full h-[350px]">
+            <div id="item1" className="carousel-item w-full">
+              <Image src="/images/home1 (1).png" alt='' width={1000} height={350} className='w-full' layout='responsive' />
+            </div>
+            <div id="item2" className="carousel-item w-full">
+              <Image src="/images/home1 (2).png" alt='' width={1000} height={350} className='w-full' layout='responsive' />
+            </div>
+          </div>
+          <div className="flex justify-center w-full py-2 gap-2">
+            <a href="#item1" className="btn btn-xs">1</a>
+            <a href="#item2" className="btn btn-xs">2</a>
+          </div>
+        </div>
         {/* data */}
         {/* <HomeData /> */}
         <div className="grid px-5 md:px-10">
