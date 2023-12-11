812a02551cf89287fe1d5a725290a2d3e6b82a53
Jaimeen Makavana <129974169+AtharvaMistry@users.noreply.github.com>
2023-12-11 22:50:23 +0530

    latest

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon.png"
+        />
+      </head>
       <body className={inter.className}>
         {/* <Header /> */}
 
