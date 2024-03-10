8c8a0d0ceb65b4c2aec689014bf250eaf426dc1e
JaimeenMakavana <jaimeen.makavana@gmail.com>
2024-03-10 15:01:55 +0530

    latest changes

--- app/page.js
+++ app/page.js
@@ -218,21 +218,20 @@ export default function Home() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
               </div>
               <div className="stat-title text-[#333333]">Total Designs</div>
-              <div className="stat-value text-primary">25.6K</div>
-              <div className="stat-desc text-[#333333]">Almost all the modern & future design</div>
+              <div className="text-[#333333] text-2xl font-semibold">25.6K</div>
             </div>
 
             <div className="stat">
               <div className="stat-figure text-secondary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
               </div>
-              <div className="stat-value text-secondary">Fast delivery</div>
+              <div className="text-[#333333] text-2xl font-semibold">Fast delivery</div>
               <div className="stat-desc text-[#333333]">Within few days</div>
             </div>
 
             <div className="stat">
               <div className="stat-value text-[#333333]">100</div>
-              <div className="stat-desc text-secondary">Minimum order quantity</div>
+              <div className="stat-desc text-[#333333]">Minimum order quantity</div>
             </div>
 
           </div>
