5f25fd551af109d94e8bbc1f318dfcd6e9776b12
JaimeenMakavana <jaimeen.makavana@gmail.com>
2024-03-10 15:27:29 +0530

    route

--- app/api/form/route.js
+++ app/api/form/route.js
@@ -2,7 +2,34 @@ import connectMongoDB from "@/libs/mongodb";
 import Form from "@/model/form";
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
+// CORS middleware
+const allowCors = (handler) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Accept, Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  return await handler(req, res);
+};
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "1mb",
+    },
+  },
+};
+
+// POST handler
+export const POST = allowCors(async (request) => {
   try {
     const { fullname, email, message } = await request.json();
     await connectMongoDB();
@@ -11,19 +38,21 @@ export async function POST(request) {
   } catch (error) {
     console.log("errrrrrrrrrrrrrrrrrrr:", error);
   }
-}
+});
 
-export async function GET() {
+// GET handler
+export const GET = allowCors(async () => {
   await connectMongoDB();
   const Forms = await Form?.find();
   return NextResponse.json({ Forms });
-}
+});
 
-export async function DELETE(request) {
+// DELETE handler
+export const DELETE = allowCors(async (request) => {
   const id = request.nextUrl.searchParams.get("id");
   console.log(id, "sdsdjhjbfsa");
   await connectMongoDB();
   const res = await Form.findByIdAndDelete(id);
   console.log(res, "response");
   return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
-}
+});
